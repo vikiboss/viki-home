caf253d6857d166d0796942c71583f40960a47e3
Viki <hi@viki.moe>
2025-01-22 22:09:06 +0800

    fix: fix responsive style

--- src/components/setting-panel/index.tsx
+++ src/components/setting-panel/index.tsx
@@ -5,7 +5,7 @@ import { LangSwitch } from './lang-switch'
 export const SettingsPanel: Component = () => {
   return (
     <div class='fixed bottom-6 w-full flex justify-center'>
-      <div class='flex justify-between justify-between w-800px md:w-92vw px-4'>
+      <div class='flex justify-between justify-between md:w-600px w-92vw px-4'>
         <div class='left-panel'>
           <ThemeSwitch />
         </div>
